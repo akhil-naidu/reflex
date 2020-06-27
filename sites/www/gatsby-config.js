b88a3bb8e7dbec9edf8c4346eecd3adbecda1e03
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2020-06-27 17:26:14 +0530

    Update gatsby-config.js

--- sites/www/gatsby-config.js
+++ sites/www/gatsby-config.js
@@ -9,6 +9,10 @@ module.exports = {
   },
   plugins: [
     `@reflexjs/gatsby-theme-base`,
+    `@reflexjs/gatsby-theme-doc`,
+    `@reflexjs/gatsby-theme-library`,
+    `@reflexjs/gatsby-theme-tutorial`,
+    `@reflexjs/gatsby-theme-styleguide`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
